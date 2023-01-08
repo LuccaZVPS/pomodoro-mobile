0044bbaa2a985bb014a4911b976a673e4d30bcba
Lucca <luccazvps@gmail.com>
2023-01-08 11:27:46 -0300

    feat: add light theme

---
+++ src/themes/light.ts
@@ -0,0 +1,7 @@
+import { DefaultTheme } from "styled-components";
+export const Light: DefaultTheme = {
+  background: "rgba(255, 76, 76, 0.10)",
+  font: "rgb(71,21,21)",
+  button: "rgba(255, 76, 76, 0.20)",
+  mainButton: "rgba(255, 76, 76, 0.70)",
+};
---
+++ src/themes/theme.d.ts
@@ -0,0 +1,10 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    background: string;
+    font: string;
+    button: string;
+    mainButton: string;
+  }
+}
