36038427a0f9dbd0c24603dd610cff84bdff512b
Lucca <luccazvps@gmail.com>
2023-01-08 21:52:10 -0300

    feat: add themes

--- src/themes/light.ts
+++ src/themes/light.ts
@@ -1,6 +1,6 @@
 import { DefaultTheme } from "styled-components";
 export const Light: DefaultTheme = {
-  background: "rgba(255, 76, 76, 0.10)",
+  background: "rgba(255, 76, 76, 0.15)",
   font: "rgb(71,21,21)",
   button: "rgba(255, 76, 76, 0.20)",
   mainButton: "rgba(255, 76, 76, 0.70)",
